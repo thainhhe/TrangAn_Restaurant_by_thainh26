bd3ff0d817be1cadeef4ce704da9bcc9738806d1
thainhhe <146052091+thainhhe@users.noreply.github.com>
2026-08-08 22:22:59 +0700

    fix: update font

--- src/App.tsx
+++ src/App.tsx
@@ -125,13 +125,13 @@ function App() {
             <p>⏰ Giờ mở cửa: 09:00 - 22:30</p>
           </div>
           <div className="footer-map">
-            <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14897.46793239677!2d105.77436295058928!3d21.017997201326157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d0d093d69%3A0x3642779b91f09f62!2zTmjDoCBIw6BuZyBUcsOgbmcgQW4!5e0!3m2!1svi!2s!4v1786199454019!5m2!1svi!2s" 
-              width="100%" 
-              height="100%" 
-              style={{ border: 0, borderRadius: '8px' }} 
-              allowFullScreen={true} 
-              loading="lazy" 
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14897.46793239677!2d105.77436295058928!3d21.017997201326157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d0d093d69%3A0x3642779b91f09f62!2zTmjDoCBIw6BuZyBUcsOgbmcgQW4!5e0!3m2!1svi!2s!4v1786199454019!5m2!1svi!2s"
+              width="100%"
+              height="100%"
+              style={{ border: 0, borderRadius: '8px' }}
+              allowFullScreen={true}
+              loading="lazy"
               referrerPolicy="strict-origin-when-cross-origin">
             </iframe>
           </div>
